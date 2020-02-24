95072a6a81bdbe3c350b488c960b214e523720ab
Jacek <Jacek@BERLIN>
2020-02-24 10:58:12 +0100

    sdfsdfsdfs

--- spring-mvc-form-validation-Udemy16/src/pl/jaceksysiak/springdemo/mvc/Customer.java
+++ spring-mvc-form-validation-Udemy16/src/pl/jaceksysiak/springdemo/mvc/Customer.java
@@ -1,5 +1,7 @@
 package pl.jaceksysiak.springdemo.mvc;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -7,9 +9,21 @@ public class Customer {
 
 	private String firstName;
 	
-	@NotNull()
+	@NotNull(message="is required")
 	@Size(min=1, message="is required")
 	private String lastName;
+
+	@Min(value=0, message="must be greater than or equal to zero")
+	@Max(value=10, message="must be less than or equal to 10")
+	private int freePasses;
+
+	public int getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
+	}
 
 	public String getFirstName() {
 		return firstName;
@@ -29,6 +43,22 @@ public class Customer {
 
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
