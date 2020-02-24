2ac4be760d451a916f115f1222574c164cc4d15e
Jacek <Jacek@BERLIN>
2020-02-24 14:57:29 +0100

    mmmm

--- spring-mvc-form-validation-Udemy16/src/pl/jaceksysiak/springdemo/mvc/Customer.java
+++ spring-mvc-form-validation-Udemy16/src/pl/jaceksysiak/springdemo/mvc/Customer.java
@@ -22,9 +22,6 @@ public class Customer {
 	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
 	private String postalCode;
 
-	
-	
-	
 
 	public Integer getFreePasses() {
 		return freePasses;
